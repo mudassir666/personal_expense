e133e13225f6b6e52d4d318f32ade319e7eac03e
mudassir666 <mudassir666666666@gmail.com>
2021-08-11 23:33:07 +0500

    dev#part5_completed

--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -1,45 +1,45 @@
-import 'package:flutter/material.dart';
-import 'package:personal_expense/models/transaction.dart';
-import 'package:personal_expense/widgets/new_transaction.dart';
-import 'package:personal_expense/widgets/transaction_list.dart';
+// import 'package:flutter/material.dart';
+// import 'package:personal_expense/models/transaction.dart';
+// import 'package:personal_expense/widgets/new_transaction.dart';
+// import 'package:personal_expense/widgets/transaction_list.dart';
 
-class UserTransaction extends StatefulWidget {
-  const UserTransaction({Key? key}) : super(key: key);
+// class UserTransaction extends StatefulWidget {
+//   const UserTransaction({Key? key}) : super(key: key);
 
-  @override
-  _UserTransactionState createState() => _UserTransactionState();
-}
+//   @override
+//   _UserTransactionState createState() => _UserTransactionState();
+// }
 
-class _UserTransactionState extends State<UserTransaction> {
+// class _UserTransactionState extends State<UserTransaction> {
   
 
-  final List<Transaction> _userTransactions = [
-    Transaction(id: "t1", title: "Shoe", amount: 99.90, date: DateTime.now()),
-    Transaction(
-        id: "t2", title: "Mobile", amount: 999.90, date: DateTime.now()),
-  ];
+//   final List<Transaction> _userTransactions = [
+//     Transaction(id: "t1", title: "Shoe", amount: 99.90, date: DateTime.now()),
+//     Transaction(
+//         id: "t2", title: "Mobile", amount: 999.90, date: DateTime.now()),
+//   ];
 
-    // final List<Map> no = [{}, {}];
+//     // final List<Map> no = [{}, {}];
 
-  void _addNewTransaction(String txTitle, double txAmount) {
-    final newTx = Transaction(
-        id: DateTime.now().toString(),
-        title: txTitle,
-        amount: txAmount,
-        date: DateTime.now());
+//   void _addNewTransaction(String txTitle, double txAmount) {
+//     final newTx = Transaction(
+//         id: DateTime.now().toString(),
+//         title: txTitle,
+//         amount: txAmount,
+//         date: DateTime.now());
 
-    setState(() {
-      _userTransactions.add(newTx);
-    });
-  }
+//     setState(() {
+//       _userTransactions.add(newTx);
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        NewTransaction(_addNewTransaction),
-        TransactionList(_userTransactions),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         NewTransaction(_addNewTransaction),
+//         TransactionList(_userTransactions),
+//       ],
+//     );
+//   }
+// }
